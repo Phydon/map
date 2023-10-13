cc21a40b23f5b9f41d789da10c13ec891055f09b
Phydon <leann.phydon@gmail.com>
2023-10-13 17:40:20 +0200

    added regex syntax information

--- src/main.rs
+++ src/main.rs
@@ -95,6 +95,9 @@ fn main() {
                     process::exit(1);
                 }
             }
+            Some(("syntax", _)) => {
+                show_regex_syntax();
+            }
             _ => {
                 unreachable!();
             }
@@ -122,7 +125,7 @@ fn manipulate_pipe() -> Command {
             "MAnipulate Pipes", "Regex syntax:", "https://docs.rs/regex/latest/regex/#syntax"
         ))
         // TODO update version
-        .version("1.0.0")
+        .version("1.1.0")
         .author("Leann Phydon <leann.phydon@gmail.com>")
         .arg_required_else_help(true)
         .arg(
@@ -153,6 +156,12 @@ fn manipulate_pipe() -> Command {
                 .short_flag('L')
                 .long_flag("log")
                 .about("Show content of the log file"),
+        )
+        .subcommand(
+            Command::new("syntax")
+                .short_flag('S')
+                .long_flag("syntax")
+                .about("Show regex syntax information"),
         )
 }
 
@@ -185,6 +194,160 @@ fn find_replace_string(
 fn find_replace_regex(input: String, re: Regex, new_pattern: &str, num_flag: u32) -> String {
     let result = re.replacen(&input, num_flag as usize, new_pattern);
     result.to_string()
+}
+
+fn show_regex_syntax() {
+    println!("{}", "Regex Syntax".bold().blue());
+    println!(
+        "More information on '{}'",
+        "https://docs.rs/regex/latest/regex/#syntax".italic()
+    );
+    println!("\n{}", "Matching one character:".bold());
+    println!(
+        r###"
+.             any character except new line (includes new line with s flag)
+[0-9]         any ASCII digit
+\d            digit (\p{{Nd}})
+\D            not digit
+\pX           Unicode character class identified by a one-letter name
+\p{{Greek}}     Unicode character class (general category or script)
+\PX           Negated Unicode character class identified by a one-letter name
+\P{{Greek}}     negated Unicode character class (general category or script)
+        "###
+    );
+    println!("\n{}", "Character classes:".bold());
+    println!(
+        r###"
+[xyz]         A character class matching either x, y or z (union).
+[^xyz]        A character class matching any character except x, y and z.
+[a-z]         A character class matching any character in range a-z.
+[[:alpha:]]   ASCII character class ([A-Za-z])
+[[:^alpha:]]  Negated ASCII character class ([^A-Za-z])
+[x[^xyz]]     Nested/grouping character class (matching any character except y and z)
+[a-y&&xyz]    Intersection (matching x or y)
+[0-9&&[^4]]   Subtraction using intersection and negation (matching 0-9 except 4)
+[0-9--4]      Direct subtraction (matching 0-9 except 4)
+[a-g~~b-h]    Symmetric difference (matching `a` and `h` only)
+[\[\]]        Escaping in character classes (matching [ or ])
+[a&&b]        An empty character class matching nothing        
+        "###
+    );
+    println!("\n{}", "Repetitions:".bold());
+    println!(
+        r###"
+x*        zero or more of x (greedy)
+x+        one or more of x (greedy)
+x?        zero or one of x (greedy)
+x*?       zero or more of x (ungreedy/lazy)
+x+?       one or more of x (ungreedy/lazy)
+x??       zero or one of x (ungreedy/lazy)
+x{{n,m}}    at least n x and at most m x (greedy)
+x{{n,}}     at least n x (greedy)
+x{{n}}      exactly n x
+x{{n,m}}?   at least n x and at most m x (ungreedy/lazy)
+x{{n,}}?    at least n x (ungreedy/lazy)
+x{{n}}?     exactly n x        
+        "###
+    );
+    println!("\n{}", "Empty matches:".bold());
+    println!(
+        r###"
+^               the beginning of a haystack (or start-of-line with multi-line mode)
+$               the end of a haystack (or end-of-line with multi-line mode)
+\A              only the beginning of a haystack (even with multi-line mode enabled)
+\z              only the end of a haystack (even with multi-line mode enabled)
+\b              a Unicode word boundary (\w on one side and \W, \A, or \z on other)
+\B              not a Unicode word boundary
+\b{{start}}, \<   a Unicode start-of-word boundary (\W|\A on the left, \w on the right)
+\b{{end}}, \>     a Unicode end-of-word boundary (\w on the left, \W|\z on the right))
+\b{{start-half}}  half of a Unicode start-of-word boundary (\W|\A on the left)
+\b{{end-half}}    half of a Unicode end-of-word boundary (\W|\z on the right)        
+        "###
+    );
+    println!("\n{}", "Grouping and flags:".bold());
+    println!(
+        r###"
+(exp)          numbered capture group (indexed by opening parenthesis)
+(?P<name>exp)  named (also numbered) capture group (names must be alpha-numeric)
+(?<name>exp)   named (also numbered) capture group (names must be alpha-numeric)
+(?:exp)        non-capturing group
+(?flags)       set flags within current group
+(?flags:exp)   set flags for exp (non-capturing)        
+        "###
+    );
+    println!("\n{}", "Flags:".bold());
+    println!(
+        r###"
+i     case-insensitive: letters match both upper and lower case
+m     multi-line mode: ^ and $ match begin/end of line
+s     allow . to match \n
+R     enables CRLF mode: when multi-line mode is enabled, \r\n is used
+U     swap the meaning of x* and x*?
+u     Unicode support (enabled by default)
+x     verbose mode, ignores whitespace and allow line comments (starting with `#`)        
+        "###
+    );
+    println!("\n{}", "Escape sequences:".bold());
+    println!(
+        r###"
+\*              literal *, applies to all ASCII except [0-9A-Za-z<>]
+\a              bell (\x07)
+\f              form feed (\x0C)
+\t              horizontal tab
+\n              new line
+\r              carriage return
+\v              vertical tab (\x0B)
+\A              matches at the beginning of a haystack
+\z              matches at the end of a haystack
+\b              word boundary assertion
+\B              negated word boundary assertion
+\b{{start}}, \<   start-of-word boundary assertion
+\b{{end}}, \>     end-of-word boundary assertion
+\b{{start-half}}  half of a start-of-word boundary assertion
+\b{{end-half}}    half of a end-of-word boundary assertion
+\123            octal character code, up to three digits (when enabled)
+\x7F            hex character code (exactly two digits)
+\x{{10FFFF}}      any hex character code corresponding to a Unicode code point
+\u007F          hex character code (exactly four digits)
+\u{{7F}}          any hex character code corresponding to a Unicode code point
+\U0000007F      hex character code (exactly eight digits)
+\U{{7F}}          any hex character code corresponding to a Unicode code point
+\p{{Letter}}      Unicode character class
+\P{{Letter}}      negated Unicode character class
+\d, \s, \w      Perl character class
+\D, \S, \W      negated Perl character class        
+        "###
+    );
+    println!("\n{}", "Perl character classes:".bold());
+    println!(
+        r###"
+\d     digit (\p{{Nd}})
+\D     not digit
+\s     whitespace (\p{{White_Space}})
+\S     not whitespace
+\w     word character (\p{{Alphabetic}} + \p{{M}} + \d + \p{{Pc}} + \p{{Join_Control}})
+\W     not word character        
+        "###
+    );
+    println!("\n{}", "ASCII character classes:".bold());
+    println!(
+        r###"
+[[:alnum:]]    alphanumeric ([0-9A-Za-z])
+[[:alpha:]]    alphabetic ([A-Za-z])
+[[:ascii:]]    ASCII ([\x00-\x7F])
+[[:blank:]]    blank ([\t ])
+[[:cntrl:]]    control ([\x00-\x1F\x7F])
+[[:digit:]]    digits ([0-9])
+[[:graph:]]    graphical ([!-~])
+[[:lower:]]    lower case ([a-z])
+[[:print:]]    printable ([ -~])
+[[:punct:]]    punctuation ([!-/:-@\[-`{{}}-~])
+[[:space:]]    whitespace ([\t\n\v\f\r ])
+[[:upper:]]    upper case ([A-Z])
+[[:word:]]     word characters ([0-9A-Za-z_])
+[[:xdigit:]]   hex digit ([0-9A-Fa-f])        
+        "###
+    );
 }
 
 fn check_create_config_dir() -> io::Result<PathBuf> {
